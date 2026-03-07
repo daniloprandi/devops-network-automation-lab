6ee3f582597dbafc0ad6854b20d7d6346fda13ba
DANILO PRANDI <daniloprandi1986@gmail.com>
2026-03-07 14:25:02 +0100

    Initial commit - DevOps Network Automation Lab

---
+++ backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "DevOps Lab API Running"
+
+@app.route("/health")
+def health():
+    return jsonify(status="ok")
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5000)
