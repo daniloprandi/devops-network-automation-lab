b93d0889dca0f4f656c3b863b3a0094d759cbc4c
DANILO PRANDI <daniloprandi1986@gmail.com>
2026-03-24 22:28:04 +0100

    Update documentation and README

--- backend/app.py
+++ backend/app.py
@@ -2,13 +2,13 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    return "DevOps Lab API Running"
+    return 'DevOps Lab API Running'
 
-@app.route("/health")
+@app.route('/health')
 def health():
-    return jsonify(status="ok")
+    return jsonify(status='ok')
 
-if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000)
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=5000)
