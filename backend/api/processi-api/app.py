d56fa0aea8f0b76fd0c91d34960f020be6775d87
DANILO PRANDI <daniloprandi1986@gmail.com>
2026-05-28 21:34:41 +0200

    processi-api riparata

--- backend/api/processi-api/app.py
+++ backend/api/processi-api/app.py
@@ -1,46 +1,92 @@
 from flask import Flask
-# importa il framework Flask
+# framework web Flask
+#
+# Flask riceve richieste HTTP
+# e gestisce le route API
 
 import os
+# modulo Python standard
+#
+# permette interazione con:
+# - filesystem
+# - processi
+# - sistema operativo Linux
+
 
 app = Flask(__name__)
-# crea l'applicazione Flask principale
-#
-# __name__:
-# nome del modulo Python corrente
-#
-# Flask usa questa informazione per:
-# - path interni 
-# - templates
-# - static files
-# - root application context
+# crea l'app Flask principale
 
 
 @app.route('/health')
-# endpoint HTTP:
+# endpoint:
 #
 # GET /health
 #
-# route minima usata come:
+# usato per:
 # - healthcheck
-# - test API
-# - verifica container attivo
-# - verifica reverse proxy nginx
+# - test container
+# - verifica nginx reverse proxy
+# - verifica runtime Flask
 
 def health():
-  # funzione eseguita
-  # quando arriva richiesta su /health
 
   return {
 
     'service': 'processi-api',
-    # nome servizio/API
+    # nome microservizio
 
     'status': 'running'
-    # stato runtime API
+    # stato runtime applicazione
   }
 
+
 @app.route('/processes')
+# endpoint:
+#
+# GET /processes
+#
+# obiettivo:
+# esporre processi Linux
+# tramite pseudo-filesystem /proc
 
 def processes():
-  os.listdir('/proc')
+
+  processi = os.listdir('/proc')
+  # legge il contenuto di /proc
+  #
+  # /proc NON è un filesystem normale
+  #
+  # è un pseudo-filesystem
+  # esposto direttamente dal kernel Linux
+  #
+  # contiene:
+  # - processi
+  # - cpu info
+  # - memoria
+  # - networking
+  # - runtime kernel
+
+  return {
+
+    'processes': processi
+    # ritorna contenuto /proc
+    # come risposta JSON HTTP
+  }
+
+
+if __name__ == '__main__':
+  # avvio locale Flask
+  #
+  # in produzione useremo Gunicorn
+
+  app.run(
+
+    host='0.0.0.0',
+    # ascolta su tutte le interfacce container
+
+    port=3140,
+    # porta runtime processi-api
+
+    debug=True
+    # debug Flask attivo
+  )
